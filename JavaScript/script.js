9572aa6181bc9d24f7c42f78dc0570df2d3234f6
shiffa-04 <shiffatariq@gmail.com>
2024-01-06 13:59:52 +0530

    This is our interactive layer

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+document.getElementById("bar").addEventListener("click",function(){
+    document.getElementById("navbar").classList.add("active");
+    
+});
+document.getElementById("close").addEventListener("click",function(){
+    document.getElementById("navbar").classList.add("hide");
+});
